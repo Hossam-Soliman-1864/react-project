f83c512537a1fb144d25c067da1322cf2d04dfaf
Hossam-Soliman-1864 <hossammohamed28394@yahoo.com>
2023-10-17 14:36:50 +0300

    task2

--- project1/src/project.js
+++ project1/src/project.js
@@ -8,6 +8,8 @@ const Tasklist = () => {
 
 const [inputValue, setInputValue] = useState('');
 
+const [editing, setEditing] = useState(false);
+
 const handleInputChange = (e) => {
 setInputValue(e.target.value);
 };
@@ -17,10 +19,34 @@ e.preventDefault();
 if (inputValue.trim()) {
   addTask(inputValue.trim());
   setInputValue('');
-}
+};};
+const handleRemoveTask = (inputValue) => {
+  const newTasks = [...tasks];
+  newTasks.splice(inputValue, 1);
+  setTasks(newTasks);
 };
+const handleEditTask = (e) => {
+  e => setNewTask(e.target.value)}
+
+
+const handleSave = (id, newTask) => {
+ // Update the task in the state
+ setTasks(prevTasks => {
+    return prevTasks.map(task => {
+      if (task.id === id) {
+        return { ...task, title: newTask };
+      }
+      return task;
+    });
+ });
+
+ // Stop editing the task
+ setEditing(false);
+};
+
+
 return (
-  <div>
+  <div className="Update">
      <form onSubmit={handleSubmit}>
        <input
          type="text"
@@ -28,14 +54,22 @@ return (
          onChange={handleInputChange}
        />
        <button type="submit">Add Task</button>
+       {/* <button type="button" onClick={() => handleRemoveTask(inputValue)}>remove Task</button> */}
+
      </form>
      <ul>
-       {tasks.map((task, index) => (
-         <li key={index}>{task}</li>
+       {tasks.map((task, inputValue) => (
+         <li key={inputValue}>{task}
+         <button onClick={() => handleRemoveTask(inputValue)}>Remove</button>
+         <button onClick={() => handleEditTask(inputValue)}>edit</button>
+         <button onClick={() => handleSave(inputValue)}>save</button></li>
+
        ))}
      </ul>
+    
   </div>
  );
 };
 
 export  default Tasklist;
+
